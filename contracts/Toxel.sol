0450dfa4aab653ac0bd69f9b85d763ed3169619a
MikkySnow <mikkysnowman@gmail.com>
2025-05-31 15:58:33 +0200

    feat: update contract

--- contracts/Toxel.sol
+++ contracts/Toxel.sol
@@ -1,78 +1,97 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.28;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Toxel is ERC721, Ownable {
-    uint256 public nextTokenId; //id токна
-    uint256 public totalSupply; //изначальное колво ток
-    uint256 public maxPixels; //максимальное колво пикс 100
-    uint256 public pixelPriceIncrementPercent; //процент
-    uint256 public initPrice; //изначальная цена
-    uint256 public pixelPrice; //конечная цена
+contract Toxel is Ownable {
 
+    uint256 public constant GRID_SIZE = 100;
+    uint256 public constant MAX_COORDINATE = GRID_SIZE - 1;
 
-    mapping(address => uint256) public userPixelCount;
-    mapping(uint256 => string) private _pixelColors;
-
-    event PixelPurchased(address indexed buyer, uint256 indexed tokenId, string color, uint256 value);
-
-    constructor(uint256 _initPrice, uint256 _totalSupply, uint256 _maxPixels, uint256 _pixelPriceIncrementPercent) ERC721('Toxel', 'TXL') Ownable(msg.sender)
-    {
-        initPrice = _initPrice;
-        nextTokenId = 1;
-        totalSupply = _totalSupply;
-        maxPixels = _maxPixels;
-        pixelPriceIncrementPercent = _pixelPriceIncrementPercent;
-    }
-
-    function buyPixel(string memory color) external payable {
-        require(msg.value == pixelPrice, "Incorrect payment amount");
-        uint256 tokenId = nextTokenId;
-
-
-        _safeMint(msg.sender, tokenId);
-        _setPixelColor(tokenId, color);
-        userPixelCount[msg.sender] += 1;
-        totalSupply += 1;
-        pixelPrice += (initPrice * pixelPriceIncrementPercent) / 100;
-
-        emit PixelPurchased(msg.sender, tokenId, color, msg.value); 
-    }
-
-    function _setPixelColor(uint256 tokenId, string memory color) internal{
-        //require(_exists(tokenId), "Token doesnt exist");
-        _pixelColors[tokenId] = color;
-    }
+    uint256 public initialPrice;
+    uint256 public priceMultiplicator;
     
-    function _getPixelColor(uint256 tokenId) external view returns(string memory){
-        return _pixelColors[tokenId];
+    address payable public fundAddress;
+
+    struct Pixel {
+        address owner;
+        uint256 price;
+        string color;
     }
 
-    function setPixelPriceIncrement(uint256 _pixelPriceIncrementPercent) external onlyOwner {
-        pixelPriceIncrementPercent = _pixelPriceIncrementPercent;
+    mapping (uint256 => mapping (uint256 => address)) private _pixelOwners;
+    mapping (uint256 => mapping (uint256 => uint256)) private _pixelPrice;
+    mapping (uint256 => mapping (uint256 => bytes3)) private _pixelColor;
+
+    event PixelChanged(address indexed newOwner, uint256 x, uint256 y, bytes3 color);
+    event PixelBought(address indexed owner, uint256 amount);
+    event PixelDividends(address indexed owner, uint256 x, uint256 y, uint256 amount);
+
+    constructor(uint256 _initPrice, uint256 _priceMultiplicator, address _fundAddress) Ownable (msg.sender) {
+        initialPrice = _initPrice;
+        priceMultiplicator = _priceMultiplicator;
+        fundAddress = _fundAddress;
     }
 
-    function check51PercentOwnership(address owner) public view returns (bool) {
-        uint256 userOwnedPixels = userPixelCount[owner];
-        uint256 totalOwnedPixels = totalSupply;
+    function claimPixel(uint256 x, uint256 y, bytes3 color) external payable {
+        address pixelOwner = getPixelOwner(x, y);
 
-        return userOwnedPixels > (totalOwnedPixels * 51) / 100;
-    }
-
-    function _getTokenIdForOwner(address owner) internal view returns (uint256) {
-        uint256 tokenId = 0;
-        for (uint256 i = 1; i <= nextTokenId; i++) {
-            if (ownerOf(i) == owner) {
-                tokenId = i;
-                break;
+        if (msg.sender == pixelOwner) {
+            // owner can change pixel color without payment
+            if (color != getPixelColor(x, y)) {
+                _pixelColor[x][y] = color;
+                emit PixelChanged(msg.sender, x, y, color);
             }
+        } else {
+            // otherwise, new owner should buy pixel
+            require(msg.value == getPixelPrice(x, y), "msg.value should be exact pixel price");
+            
+            if (pixelOwner != address(0)) {
+                // send funds to previous owner. Potential re-entrancy
+                payable(pixelOwner).call{value: getPixelPrice(x, y)}();
+                emit PixelDividends(pixelOwner, x, y, getPixelPrice(x, y));
+            } else {
+                fundAddress.call{value: getPixelPrice(x, y)}();
+            }
+
+            // update pixel details
+            _pixelPrice[x][y] = getPixelPrice(x, y) * priceMultiplicator;
+            _pixelOwner[x][y] = msg.sender;
+
+            if (getPixelColor(x, y) != color) {
+                _pixelColor[x][y] = color;
+                emit PixelChanged(msg.sender, x, y, color);
+            }
+
+            emit PixelBought(msg.sender, x, y, color);
         }
-        return tokenId;
     }
 
+    function setPriceMultiplicator(uint256 _priceMultiplicator) public onlyOwner() {
+        priceMultiplicator = _priceMultiplicator;
+    }
 
+    function setFundAddress(address payable _fundAddress) public onlyOwner {
+        fundAddress = _fundAddress;
+    }
+
+    function setInitialPrice(uint256 _initialPrice) public onlyOwner() {
+        initialPrice = _initialPrice;
+    }
+
+    function getPixelOwner(uint256 x, uint256 y) public view returns (address) {
+        return _pixelOwners[x][y];
+    } 
+
+    function getPixelColor(uint256 x, uint256 y) public view returns (bytes3) {
+        return _pixelColor[x][y];
+    } 
+
+    function getPixelPrice(uint256 x, uint256 y) public view returns (uint256) {
+        if getPixelOwner(x, y) == address(0) {
+            return initialPrice;
+        }
+
+        return _pixelPrice[x][y];
+    } 
 }
-//минт
-//если ты владелец пиккселя можешь перекрасить бесплатно
